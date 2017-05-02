1ba3a45076ecaab1402cd786a946cff67f3cf89d
Nick Maludy <nick.maludy@encore.tech>
2017-05-02 07:33:02 -0400

    Changed around the subtree helper script(s)

---
+++ bin/subtree-remote.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git remote add subtree/lib/py-menandmice git@github.com:EncoreTechnologies/py-menandmice.git
+git remote add subtree/packs/stackstorm-menandmice git@github.com:EncoreTechnologies/stackstorm-menandmice.git
