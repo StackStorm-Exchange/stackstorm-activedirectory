901bfce812a6c86cca203cbfd674e23c66a9af36
Nick Maludy <nick.maludy@encore.tech>
2017-05-02 12:55:37 -0400

    Added remote for packs/stackstorm-activedirectory

--- bin/subtree-remote.sh
+++ bin/subtree-remote.sh
@@ -2,3 +2,4 @@
 
 git remote add subtree/lib/py-menandmice git@github.com:EncoreTechnologies/py-menandmice.git
 git remote add subtree/packs/stackstorm-menandmice git@github.com:EncoreTechnologies/stackstorm-menandmice.git
+git remote add subtree/packs/stackstorm-activedirectory git@github.com:EncoreTechnologies/stackstorm-activedirectory.git
