24e79b142c2664ece382f360505526c10e9c1bc0
Nick Maludy <nmaludy@gmail.com>
2017-06-08 08:57:06 -0400

    CLOUD-423 #comment Added several more unit tests for run_ad_cmdlet. Made the args parameter not required. Updated some descriptions in the action parameters

--- packs/stackstorm-activedirectory/actions/lib/action.py
+++ packs/stackstorm-activedirectory/actions/lib/action.py
@@ -22,14 +22,20 @@ class BaseAction(Action):
             creds[key] = self.resolve_creds_spec(value)
         return creds
 
+    def get_arg(self, key, **kwargs):
+        if key in kwargs:
+            return kwargs[key]
+        else:
+            return None
+
     def create_creds_spec(self, **kwargs):
-        return {'connect': {'credential_name': kwargs['credential_name'],
-                            'username': kwargs['username'],
-                            'password': kwargs['password'],
+        return {'connect': {'credential_name': self.get_arg('credential_name', **kwargs),
+                            'username': self.get_arg('username', **kwargs),
+                            'password': self.get_arg('password', **kwargs),
                             'required': True},
-                'cmdlet': {'credential_name': kwargs['cmdlet_credential_name'],
-                           'username': kwargs['cmdlet_username'],
-                           'password': kwargs['cmdlet_password'],
+                'cmdlet': {'credential_name': self.get_arg('cmdlet_credential_name', **kwargs),
+                           'username': self.get_arg('cmdlet_username', **kwargs),
+                           'password': self.get_arg('cmdlet_password', **kwargs),
                            'required': False}}
 
     def resolve_creds_spec(self, creds_spec):
@@ -57,7 +63,7 @@ class BaseAction(Action):
             if item in creds_spec and creds_spec[item]:
                 # use creds from cmdline first (override)
                 creds[item] = creds_spec[item]
-            elif item in config_creds and config_creds[item]:
+            elif config_creds and item in config_creds and config_creds[item]:
                 # fallback to creds in config
                 creds[item] = config_creds[item]
 
@@ -84,16 +90,13 @@ class BaseAction(Action):
     def default_port():
         return 5986
 
-    def resolve_transport(self, transport, port, connect_creds):
+    def resolve_transport(self, connect_creds, **kwargs):
         """ Resolves the transport and port to use for the connection
         based on the following priorities:
         1) if port/transport specified as action params, use this
         2) if port/transport specified as params on the credentials in config
         3) if port/transport specified at root level in the config
         4) else, use the default port/transport (5986/ntlm)
-        :param transport: the transport parameter given to the action
-        (default = None)
-        :param port: the port parameter given to the action (default = None)
         :param connect_creds: the connection credentials read in from the
         config.
         :returns: dictionary with 'transport' and 'port' set the resolved
@@ -103,8 +106,8 @@ class BaseAction(Action):
         resolved_transport = None
         resolved_port = None
 
-        if port:
-            resolved_port = port
+        if 'port' in kwargs:
+            resolved_port = kwargs['port']
         elif 'port' in connect_creds:
             resolved_port = connect_creds['port']
         elif 'port' in self.config:
@@ -112,8 +115,8 @@ class BaseAction(Action):
         else:
             resolved_port = BaseAction.default_port()
 
-        if transport:
-            resolved_transport = transport
+        if 'transport' in kwargs:
+            resolved_transport = kwargs['transport']
         elif 'transport' in connect_creds:
             resolved_transport = connect_creds['transport']
         elif 'transport' in self.config:
@@ -133,20 +136,17 @@ class BaseAction(Action):
 
     def run_ad_cmdlet(self, cmdlet, **kwargs):
         creds = self.resolve_creds(**kwargs)
-        tport = self.resolve_transport(kwargs['transport'],
-                                       kwargs['port'],
-                                       creds['connect'])
+        tport = self.resolve_transport(creds['connect'], **kwargs)
         powershell = ''
-        cmdlet_args = kwargs['args']
-        if 'username' in creds['cmdlet'] and 'passowrd' in creds['cmdlet']:
-            powershell = '''
-                $securepass = ConvertTo-SecureString "{3}" -AsPlainText -Force;
-                $admincreds = New-Object System.Management.Automation.PSCredential("{2}", $securepass);
-                {0} -Credential $admincreds {1}
-                '''.format(cmdlet,  # noqa (this suppresses long line warnings above)
-                           cmdlet_args,
-                           creds['cmdlet']['username'],
-                           creds['cmdlet']['password'])
+        cmdlet_args = kwargs['args'] if 'args' in kwargs else ''
+        if 'username' in creds['cmdlet'] and 'password' in creds['cmdlet']:
+            powershell = ("$securepass = ConvertTo-SecureString \"{3}\" -AsPlainText -Force;\n"
+                          "$admincreds = New-Object System.Management.Automation.PSCredential(\"{2}\", $securepass);\n"  # noqa
+                          "{0} -Credential $admincreds {1}"
+                          "").format(cmdlet,
+                                     cmdlet_args,
+                                     creds['cmdlet']['username'],
+                                     creds['cmdlet']['password'])
         else:
             powershell = '{0} {1}'.format(cmdlet, cmdlet_args)
 
