60f8e3ac4b56560867047f4080e49d6b202f8c70
Brian Tackett <briantac@buffalo.edu>
2019-07-31 12:40:43 -0400

    ST2-166 debug

--- sensors/get_alerts.py
+++ sensors/get_alerts.py
@@ -74,6 +74,8 @@ class ADAdminSensor(PollingSensor):
 
             self._logger.info(response)
             self._logger.info("here")
+            self._logger.info(dir(response))
+            self._logger.info("hereeee")
             self._logger.info(response.json())
 
             removed = list(set(members) - set(response))
