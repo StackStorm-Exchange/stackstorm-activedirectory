a78b5f04b19f6562cbe503afc2d8295f9cb59c77
Brian Tackett <briantac@buffalo.edu>
2019-08-02 15:46:37 -0400

    ST2-166 json debug

--- sensors/get_alerts.py
+++ sensors/get_alerts.py
@@ -73,8 +73,6 @@ class ADAdminSensor(PollingSensor):
             response = self.session.run_ps(powershell)
 
             self._logger.info(response)
-            self._logger.info(response.__dict__)
-            self._logger.info(response.json())
 
             response_output = response.__dict__['std_out']
 
