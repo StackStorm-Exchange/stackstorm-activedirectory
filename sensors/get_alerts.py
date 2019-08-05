948aaa0935a8da09144885fa6cdeb4c5d60ec90d
Brian Tackett <briantac@buffalo.edu>
2019-08-05 14:27:00 -0400

    ST2-166 separate triggers; handle multiple groups

--- sensors/get_alerts.py
+++ sensors/get_alerts.py
@@ -17,7 +17,6 @@ class ADAdminSensor(PollingSensor):
         super(ADAdminSensor, self).__init__(sensor_service=sensor_service,
                                             config=config,
                                             poll_interval=poll_interval)
-        self._trigger_ref = 'activedirectory.watched_group_changed'
 
         self._logger = self._sensor_service.get_logger(__name__)
 
@@ -41,7 +40,10 @@ class ADAdminSensor(PollingSensor):
                                      transport=transport,
                                      server_cert_validation='ignore')
 
-        self.members = ['first_run_place_holder']
+        self.members = {}
+
+        for group in self.groups:
+            self.members[group] = ['first_run_place_holder']
 
     def setup(self):
         pass
@@ -82,31 +84,46 @@ class ADAdminSensor(PollingSensor):
 
             removed = []
             added = []
+            added_names = []
+            removed_names = []
 
             for new_item in response_list:
                 if new_item not in members:
                     added.append(new_item)
+                    added_names.append(new_item.get('SamAccountName'))
             for old_item in members:
                 if old_item not in response_list:
                     removed.append(old_item)
+                    removed_names.append(old_item.get('SamAccountName'))
 
-            if removed or added:
-                self._logger.info('Change in AD group membership detected.')
+            if added:
+                self._logger.info('New member in AD group detected.')
                 payload = {
-                    'old_list': members,
-                    'new_list': response_list,
-                    'removed': removed,
                     'added': added,
                     'group': group,
-                    'tenant': self.creds_name
+                    'tenant': self.creds_name,
+                    'SamAccountNames': added_names
                 }
 
-                self.sensor_service.dispatch(trigger=self._trigger_ref,
+                self.sensor_service.dispatch(trigger='activedirectory.watched_group_member_added',
                                              payload=payload)
 
-                self._set_members(members=response_list)
-            else:
+            if removed:
+                self._logger.info('Member removal in AD group detected.')
+                payload = {
+                    'removed': removed,
+                    'group': group,
+                    'tenant': self.creds_name,
+                    'SamAccountNames': removed_names
+                }
+
+                self.sensor_service.dispatch(trigger='activedirectory.watched_group_member_removed',
+                                             payload=payload)
+
+            if not removed and not added:
                 self._logger.info('No change in AD group membership detected')
+
+            self._set_members(members=response_list, group=group)
 
     def cleanup(self):
         pass
@@ -120,14 +137,14 @@ class ADAdminSensor(PollingSensor):
     def remove_trigger(self, trigger):
         pass
 
-    def _get_members(self):
-        if not self.members and hasattr(self.sensor_service, 'get_value'):
-            self.members = self.sensor_service.get_value('members')
+    def _get_members(self, group):
+        if not self.members.get(group) and hasattr(self.sensor_service, 'get_value'):
+            self.members[group] = self.sensor_service.get_value(group + '.members')
 
         return self.members
 
-    def _set_members(self, members):
-        self.members = members
+    def _set_members(self, members, group):
+        self.members[group] = members
 
         if hasattr(self.sensor_service, 'set_value'):
-            self.sensor_service.set_value(name='members', value=members)
+            self.sensor_service.set_value(name=group + '.members', value=members)
