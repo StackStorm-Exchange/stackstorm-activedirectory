31f16dae5442d07bc92237211c3e8551a4c1579a
Brian Tackett <briantac@buffalo.edu>
2019-08-05 15:33:54 -0400

    ST2-166 debug

--- sensors/get_alerts.py
+++ sensors/get_alerts.py
@@ -53,6 +53,7 @@ class ADAdminSensor(PollingSensor):
         for group in self.groups:
 
             members = self._get_members(group)
+            self._logger.info(group)
             self._logger.info('members')
             self._logger.info(members)
 
